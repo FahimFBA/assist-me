21a61e69fa8aeccdc680e4599bd95736623c0534
Joy Shaheb <khondokoralam@gmail.com>
2023-10-12 01:28:51 +0600

    react router dom added

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Home from "./Home";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import Signup from "./Signup";
+import ResetPassword from "./ResetPassword";
+import ForgotPassword from "./ForgotPassword";
+import ErrorPage from "./ErrorPage";
+
+export {
+  Home,
+  Signup,
+  Login,
+  ResetPassword,
+  ForgotPassword,
+  Dashboard,
+  ErrorPage,
+};
