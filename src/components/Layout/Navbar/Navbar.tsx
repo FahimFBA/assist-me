8cb16b0eb0abf070ef29cd51dd461493877a9d3b
Joy Shaheb <khondokoralam@gmail.com>
2023-10-20 12:34:00 +0600

    auth pages added

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import CustomNavlink from "./CustomNavlink";
+
 const Navbar = () => {
   return (
     <nav
@@ -6,9 +9,9 @@ const Navbar = () => {
     >
       <div className="container">
         {/* <!-- Logo --> */}
-        <a className="navbar-brand" href="index.html">
+        <Link className="navbar-brand" to="/">
           <img src="/images/logo.svg" alt="" width="165" />
-        </a>
+        </Link>
 
         {/* <!-- Navbar toggler button --> */}
         <button
@@ -31,194 +34,16 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarContent">
           <div className="navbar-content-inner ms-lg-auto d-flex flex-column flex-lg-row align-lg-center gap-4 gap-lg-10 p-2 p-lg-0">
             <ul className="navbar-nav gap-lg-2 gap-xl-5">
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle active"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="index.html">
-                      Home one
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="index-lite.html">
-                      Home one lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="index-2.html">
-                      Home two
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="index-2-lite.html">
-                      Home two lite
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Pages
-                </a>
-                <ul className="dropdown-menu megamenu megamenu-cols-2">
-                  <li>
-                    <a className="dropdown-item " href="about.html">
-                      About
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="about-lite.html">
-                      About lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="contact.html">
-                      Contact
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="contact-lite.html">
-                      Contact lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="blog.html">
-                      Blog
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="blog-lite.html">
-                      Blog lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="article.html">
-                      Article
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="article-lite.html">
-                      Article lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="use-cases.html">
-                      Use cases
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="use-cases-lite.html">
-                      Use cases lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="use-cases-details.html">
-                      Case details
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item "
-                      href="use-cases-details-lite.html"
-                    >
-                      Case details lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="pricing-plan.html">
-                      Pricing
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="pricing-plan-lite.html">
-                      Pricing lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="login.html">
-                      Login
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="login-lite.html">
-                      Login lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="register.html">
-                      Register
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="register-lite.html">
-                      Register lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="forgot-password.html">
-                      Forgot password
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item"
-                      href="forgot-password-lite.html"
-                    >
-                      Forgot password lite
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="404.html">
-                      404
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item " href="404-lite.html">
-                      404 lite
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " href="use-cases.html">
-                  Use cases
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " href="pricing-plan.html">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " href="contact.html">
-                  Contact
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="login.html">
-                  Login
-                </a>
-              </li>
+              <CustomNavlink label="Home" to="/" />
+              <CustomNavlink label="Use Cases" to="/use-cases" />
+              <CustomNavlink label="Pricing" to="/pricing" />
+              <CustomNavlink label="Contact" to="/contact" />
+              <CustomNavlink label="Login" to="/login" />
             </ul>
             <div className="">
-              <a href="login.html" className="btn btn-outline-primary-dark">
+              <Link to="/signup" className="btn btn-outline-primary-dark">
                 Get started
-              </a>
+              </Link>
             </div>
           </div>
         </div>
