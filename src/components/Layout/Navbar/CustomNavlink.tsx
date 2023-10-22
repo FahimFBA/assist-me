0156b825572b0cb1871fd96ac6beb4194cccd876
John Doe <mitka_j@yahoo.com>
2023-10-22 14:00:56 +0200

    bugfix: navbar click issue fixed

--- src/components/Layout/Navbar/CustomNavlink.tsx
+++ src/components/Layout/Navbar/CustomNavlink.tsx
@@ -1,12 +1,21 @@
 import { NavLink } from "react-router-dom";
 
-const CustomNavlink = ({ label, to }: { label: string; to: string }) => {
+const CustomNavlink = ({
+  label,
+  to,
+  onClick,
+}: {
+  label: string;
+  to: string;
+  onClick?: () => void;
+}) => {
   return (
     <NavLink
       className={({ isActive, isPending }) =>
         isPending ? "nav-link" : isActive ? "nav-link active" : "nav-link"
       }
       to={to}
+      onClick={onClick}
     >
       {label}
     </NavLink>
