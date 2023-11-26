6f0af34c0514be3d45bda5a5a91d96ab6be01396
Joy Shaheb <khondokoralam@gmail.com>
2023-11-26 17:42:37 +0600

    new styles added

--- src/pages/utils/AuthenticationRoutes.tsx
+++ src/pages/utils/AuthenticationRoutes.tsx
@@ -3,11 +3,6 @@ import { RootState } from "../../store";
 import { useSelector } from "react-redux";
 import { Navbar, Footer } from "../../components/Layout";
 
-// import "bootstrap/dist/css/bootstrap.css";
-// import "bootstrap/dist/js/bootstrap.js";
-// import "../../styles/plugin.css";
-// import "../../styles/style.css";
-
 const AuthenticationRoutes = () => {
   const userUid = useSelector((state: RootState) => state.user.uid);
   return userUid ? (
