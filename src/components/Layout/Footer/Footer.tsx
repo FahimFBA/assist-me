bbe90b1f5b75908e5089baad439fd970e032e602
Joy Shaheb <khondokoralam@gmail.com>
2023-12-06 18:22:51 +0600

    home page new ui added

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,134 +1,37 @@
-import {
-  FaLocationArrow,
-  FaGithub,
-  FaDribbble,
-  FaFacebookSquare,
-  FaTwitter,
-  FaInstagram,
-} from "react-icons/fa";
-import Logo from "../../Logo";
+import { FacebookIcon, LinkedinIcon, TwitterIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="container">
-        <div className="row g-10">
-          <div className="col-lg-9 col-xl-8 order-lg-2">
-            <div className="row g-6">
-              <div className="col-md-4 col-lg-4">
-                <div className="footer-widget text-center text-md-start">
-                  <h6 className="text-white mb-2">AssistMe</h6>
-                  <ul className="link-list list-unstyled mb-0">
-                    <li>
-                      <a href="about.html">About</a>
-                    </li>
-                    <li>
-                      <a href="blog.html">Blog</a>
-                    </li>
-                    <li>
-                      <a href="login.html">Sign in</a>
-                    </li>
-                    <li>
-                      <a href="register.html">Register</a>
-                    </li>
-                    <li>
-                      <a href="contact.html">Contact</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md-4 col-lg-4">
-                <div className="footer-widget text-center text-md-start">
-                  <h6 className="text-white mb-2">Use Cases</h6>
-                  <ul className="link-list list-unstyled mb-0">
-                    <li>
-                      <a href="use-cases-details.html">AI writer</a>
-                    </li>
-                    <li>
-                      <a href="use-cases-details.html">Businesses AI</a>
-                    </li>
-                    <li>
-                      <a href="use-cases-details.html">AI Blog writer</a>
-                    </li>
-                    <li>
-                      <a href="use-cases-details.html">AI Content Creator</a>
-                    </li>
-                    <li>
-                      <a href="use-cases-details.html">Article write</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md-4 col-lg-4">
-                <div className="footer-widget text-center text-md-start">
-                  <h6 className="text-white mb-4">News & Update</h6>
-                  <form action="#">
-                    <div className="input-group">
-                      <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter your email"
-                      />
-                      <button
-                        className="btn btn-primary-dark px-4"
-                        type="button"
-                      >
-                        <FaLocationArrow strokeWidth={1.5} />
-                      </button>
-                    </div>
-                  </form>
-                  <ul className="list-unstyled d-flex flex-wrap align-center justify-center justify-md-start gap-3 social-list mb-0 mt-5">
-                    <li>
-                      <a href="#">
-                        <FaGithub strokeWidth={1.5} />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <FaDribbble strokeWidth={1.5} />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <FaFacebookSquare strokeWidth={1.5} />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <FaTwitter strokeWidth={1.5} />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <FaInstagram strokeWidth={1.5} />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-3 order-lg-1 me-auto">
-            <div className="footer-widget text-center text-lg-start">
-              <Link to="/">
-                <Logo />
-              </Link>
-              <p className="fs-sm mb-0 mt-4">
-                A Magical Tool to Optimize you content for the first know who
-                you're targeting. Identify your target audience.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="text-center py-6 mt-8">
-          <p className="fs-sm mb-0">
-            Copyright <span className="text-primary-dark">AssistMe</span>. Design
-            By Team Iphim
-          </p>
+    <footer className="py-3 pb-6 px-4 md:px-6 bg-gray-200 dark:bg-gray-800">
+      <div className="container mx-auto max-w-7xl flex flex-col gap-2 sm:flex-row py-6 w-full items-center">
+        <h3 className="text-2xl">Assist Me</h3>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <Link className="text-xs hover:underline underline-offset-4" to="#">
+            Privacy Policy
+          </Link>
+          <Link className="text-xs hover:underline underline-offset-4" to="#">
+            Terms of Service
+          </Link>
+          <Link className="text-xs hover:underline underline-offset-4" to="#">
+            Contact Us
+          </Link>
+        </nav>
+        <div className="flex gap-4 mt-4 sm:mt-0">
+          <Link to="#">
+            <TwitterIcon className="h-5 w-5" />
+          </Link>
+          <Link to="#">
+            <FacebookIcon className="h-5 w-5" />
+          </Link>
+          <Link to="#">
+            <LinkedinIcon className="h-5 w-5" />
+          </Link>
         </div>
       </div>
+      <p className="text-xs text-gray-500 dark:text-gray-400 text-center">
+        © AssistMe. All rights reserved.
+      </p>
     </footer>
   );
 };
