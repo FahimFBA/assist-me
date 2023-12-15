5cbfb753d45e758484151ebb973f10863e1adb61
FahimFBA <fahimbinamin@gmail.com>
2023-12-15 21:24:05 +0600

    change: landing page initial data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,12 +8,23 @@ const Home = () => {
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6 text-center">
+            {/* <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none"> */}
+
+            {/* 
+            text-2xl is the default font size.
+            sm:text-3xl will be applied on small screens and above (sm: breakpoint).
+            md:text-4xl will be applied on medium screens and above (md: breakpoint).
+            lg:text-5xl will be applied on large screens and above (lg: breakpoint). */}
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-              Learn how to use our App
+              Manage Your Tasks and Emails in One Place
+              <br />
+              <br />
+              Effortlessly!
             </h1>
+            <br />
+            <br />
             <p className="text-gray-500 mt-2 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-              Our comprehensive guide helps you get started with our mobile
-              application in no time.
+              Streamline Your Day with Integrated Email and Todo List Management!
             </p>
             <Link to="/signup">
               <Button className="mt-4" variant="secondary">
